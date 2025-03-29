29cb893a3dd40c663b1b9b34fbecdbf967b089d4
Vitaly Turovsky <vital2580@icloud.com>
2025-03-29 09:50:34 +0300

    add proxy shutdown message and parsing fn

--- browser.js
+++ browser.js
@@ -393,6 +393,10 @@ Socket.prototype._connectWebSocket = function (token, cb) {
 	}
 };
 
+Socket.prototype.handleStringMessage = function (message) {
+	return true
+}
+
 Socket.prototype._handleWebsocket = function () {
 	var self = this;
 
@@ -426,8 +430,10 @@ Socket.prototype._handleWebsocket = function () {
 				self.emit('pong', contents.slice('pong:'.length));
 				return
 			}
-			var buffer = new Buffer(contents);
-			gotBuffer(buffer);
+			if (self.handleStringMessage(contents)) {
+				var buffer = new Buffer(contents);
+				gotBuffer(buffer);
+			}
 		} else if (window.Blob && contents instanceof Blob) {
 			var fileReader = new FileReader();
 			let resolveReading
